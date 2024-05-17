5272bf394b6bb944eb1039606ce66ef60958d6ee
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-17 11:25:25 +0900

    feat : user method

--- src/main/java/com/buckshot/Item.java
+++ src/main/java/com/buckshot/Item.java
@@ -1,7 +1,12 @@
 package com.buckshot;
 
 public class Item {
+    String name;
     public void use(){
         return;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/com/buckshot/User.java
+++ src/main/java/com/buckshot/User.java
@@ -2,6 +2,7 @@ package com.buckshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class User {
     private int health;
@@ -9,13 +10,54 @@ public class User {
     private List<Item> items = new ArrayList<Item>();
     private Gun gun;
     private User enemy;
-    private boolean isFree;
+    private Boolean isFree;
+    private Boolean myTurn;
 
-    public void useItem(Item item){
+    public void useItem(int index){
+        Item i = items.get(index -1);
+        System.out.println(this.name+"이 "+ i.getName()+"을 사용했습니다.");
+        i.use();
+        items.remove(index);
         return;
     }
 
     public void myTurn(){
+        // 1. 아이템 사용 // 2. 나에게 쏘기 // 3. 적에게 쏘기
+        System.out.println(this.name + "의 차례입니다.\n");
+
+        if(!isFree){
+            System.out.println(this.name + "이 수갑에 묶여있어 차례가 넘어갑니다.\n");
+            return;
+        }
+        Scanner sc = new Scanner(System.in);
+
+        while(myTurn){
+            System.out.println("1. 아이템 사용 2. 나에게 쏘기 3. 적에게 쏘기\n");
+            System.out.println("액션을 선택하세요 > ");
+            int option = sc.nextInt();
+            switch (option){
+                case 1:
+                    System.out.println("아이템을 선택하세요 > ");
+                    int itemIndex = sc.nextInt();
+                    useItem(itemIndex);
+                    break;
+                case 2:
+                    System.out.println(this.name +"가 자신에게 총을 쏩니다.");
+                    gun.shoot(this);
+                    break;
+                case 3:
+                    System.out.println(this.name +"이 "+ enemy.name+"에게 총을 쏩니다.");
+                    gun.shoot(enemy);
+                    setMyTurn(false);
+                    break;
+                default:
+                    System.out.println("잘못된 입력입니다.");
+                    break;
+            }
+        }
+    }
+
+    private void printItem(){
 
     }
 
@@ -65,5 +107,21 @@ public class User {
 
     public void setFree(boolean free) {
         isFree = free;
+    }
+
+    public Boolean getFree() {
+        return isFree;
+    }
+
+    public void setFree(Boolean free) {
+        isFree = free;
+    }
+
+    public Boolean getMyTurn() {
+        return myTurn;
+    }
+
+    public void setMyTurn(Boolean myTurn) {
+        this.myTurn = myTurn;
     }
 }
