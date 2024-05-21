c765363db659daff94f588faba2a3e15a4b4d0df
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-21 12:06:47 +0900

    feat : edit Handcuff

--- src/main/java/com/buckshot/Items/Handcuff.java
+++ src/main/java/com/buckshot/Items/Handcuff.java
@@ -3,19 +3,26 @@ package com.buckshot.Items;
 import com.buckshot.Core.User;
 
 public class Handcuff extends UserItem {
+    private String name;
     public Handcuff(User target) {
         super(target);
         this.name = "수갑  ";
+        this.description = "상대방이 다음턴을 못하도록 막습니다.";
     }
 
     @Override
-    public void use(){
-        lockUser();
+    public void useUser(User user){
+        lockUser(user);
     }
 
-    public void lockUser(){
-        target.setFree(false);
-        System.out.println(target.getName()+"가 수갑에 묶였습니다!\n");
-        return;
+    @Override
+    public void describeUser(){
+        System.out.println(this.name+"가 수갑에 묶였습니다!\n");
     }
+
+    public void lockUser(User user){
+        user.setFree(false);
+        this.name = user.getName();
+    }
+
 }
