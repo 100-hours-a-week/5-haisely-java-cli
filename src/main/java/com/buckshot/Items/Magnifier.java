64fc94f490757c21214f31c4050cebb9b21cfa0e
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-21 11:57:05 +0900

    feat : edit Magnifier

--- src/main/java/com/buckshot/Items/Magnifier.java
+++ src/main/java/com/buckshot/Items/Magnifier.java
@@ -3,22 +3,27 @@ package com.buckshot.Items;
 import com.buckshot.Core.Gun;
 
 public class Magnifier extends GunItem {
+    private boolean isReal;
     public Magnifier(Gun gun) {
         super(gun);
         this.name = "돋보기";
     }
 
     @Override
-    public void use(){
-        checkReal();
+    public void useGun(Gun gun){
+        checkReal(gun);
     }
 
-    public void checkReal(){
-        if (gun.isReal()){
+    @Override
+    public void describeGun(){
+        if (this.isReal){
             System.out.println("실탄이 장착되어 있습니다.\n");
         } else {
             System.out.println("공포탄이 장착되어 있습니다.\n");
         }
     }
 
+    public void checkReal(Gun gun){
+        this.isReal = gun.isReal();
+    }
 }
