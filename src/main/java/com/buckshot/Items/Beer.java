06c4ecc4735adfce509397929781820dda228c21
Haisley Jang <98401161+Ssun2zang@users.noreply.github.com>
2024-05-21 12:02:20 +0900

    feat: edit beer

--- src/main/java/com/buckshot/Items/Beer.java
+++ src/main/java/com/buckshot/Items/Beer.java
@@ -3,22 +3,28 @@ package com.buckshot.Items;
 import com.buckshot.Core.Gun;
 
 public class Beer extends GunItem {
+    private int removed;
     public Beer(Gun gun) {
         super(gun);
         this.name = "맥주  ";
+        this.description = "장전된 총알을 버립니다.";
     }
 
     @Override
-    public void use(){
+    public void useGun(Gun gun){
         dumpBullet();
     }
 
-    public void dumpBullet(){
-        int b = gun.removeBullet();
-        if (b!=0){
+    @Override
+    public void describeGun(){
+        if (this.removed!=0){
             System.out.println("실탄이 제거되었습니다.\n");
         } else {
             System.out.println("공포탄이 제거되었습니다\n");
         }
+    }
+
+    public void dumpBullet(){
+        this.removed = gun.removeBullet();
     }
 }
